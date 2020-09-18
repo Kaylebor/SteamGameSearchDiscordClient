395a4c1080c922544c67021eb1824eacca473edd
Ender Veiga Bueno <veigabuenoender@gmail.com>
2020-09-18 13:09:14 +0200

    Removed token from code

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
+from os import environ
 from SteamGameSearchDiscordClient import SteamGameSearchDiscordClient
 from TasksCog import TasksCog
 
 client = SteamGameSearchDiscordClient()
 taskscog = TasksCog(client)
-client.run('NzU2NDM3MTE3NzIyMTY1MzU5.X2R09g.SOQjRZgq1ClT7r0dXdo4CH9dzHw')
+client.run(environ['STEAM_GAME_SEARCH_DISCORD_TOKEN'])
